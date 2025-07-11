f783ecd346e53962ef36ebd4bff0b5c3f069d821
liferecords <yjmm10@yeah.net>
2025-07-11 11:58:54 +0800

    更新设置文件，添加新的 Bash 命令选项；在 Jest 设置文件中模拟 Electron 相关模块以支持测试。

--- test/setup.js
+++ test/setup.js
@@ -1,3 +1,35 @@
 // Jest setup file
 global.TextEncoder = require('util').TextEncoder;
 global.TextDecoder = require('util').TextDecoder;
+
+// Mock Electron
+jest.mock('electron', () => ({
+  app: {
+    quit: jest.fn(),
+    on: jest.fn(),
+    whenReady: jest.fn(() => Promise.resolve()),
+    getPath: jest.fn(() => '/tmp')
+  },
+  BrowserWindow: jest.fn().mockImplementation(() => ({
+    loadFile: jest.fn(() => Promise.resolve()),
+    on: jest.fn(),
+    show: jest.fn(),
+    destroy: jest.fn(),
+    webContents: {
+      getURL: jest.fn(() => 'file:///src/index.html'),
+      on: jest.fn()
+    }
+  })),
+  ipcMain: {
+    on: jest.fn(),
+    handle: jest.fn()
+  },
+  Menu: {
+    setApplicationMenu: jest.fn()
+  },
+  Tray: jest.fn().mockImplementation(() => ({
+    setToolTip: jest.fn(),
+    setContextMenu: jest.fn(),
+    on: jest.fn()
+  }))
+}));
